078571b8aa92ad06d9cd15f9ade146a686039cf6
patshone <patshone@gmail.com>
2010-05-30 15:29:34 +0100

    Updates to support multiple accounts in a single ofx file

--- lib/ofx/parser/ofx102.rb
+++ lib/ofx/parser/ofx102.rb
@@ -12,7 +12,9 @@ module OFX
       TRANSACTION_TYPES = {
         "CREDIT" => :credit,
         "DEBIT" => :debit,
-        "OTHER" => :other
+        "OTHER" => :other,
+        "POS" => :pos,
+        "ATM" => :atm
       }
       
       attr_reader :headers
@@ -25,24 +27,30 @@ module OFX
         @html = Nokogiri::HTML.parse(body)
       end
       
-      def account
-        @account ||= build_account
+      def accounts
+        @accounts = build_accounts
       end
       
       private
-        def build_account
+        def build_accounts
+          html.search("bankmsgsrsv1 > stmttrnrs").collect do |account|
+            build_account(account)
+          end
+        end
+           
+        def build_account(account)
           OFX::Account.new({
-            :bank_id      => html.search("bankacctfrom > bankid").inner_text,
-            :id           => html.search("bankacctfrom > acctid").inner_text,
-            :type         => ACCOUNT_TYPES[html.search("bankacctfrom > accttype").inner_text],
-            :transactions => build_transactions,
-            :balance      => build_balance,
-            :currency     => html.search("bankmsgsrsv1 > stmttrnrs > stmtrs > curdef").inner_text
+            :bank_id      => account.search("bankacctfrom > bankid").inner_text,
+            :id           => account.search("bankacctfrom > acctid").inner_text,
+            :type         => ACCOUNT_TYPES[account.search("bankacctfrom > accttype").inner_text],
+            :transactions => build_transactions(account),
+            :balance      => build_balance(account),
+            :currency     => account.search("stmtrs > curdef").inner_text
           })
         end
         
-        def build_transactions
-          html.search("banktranlist > stmttrn").collect do |element|
+        def build_transactions(account)
+          account.search("banktranlist > stmttrn").collect do |element|
             build_transaction(element)
           end
         end
@@ -73,13 +81,13 @@ module OFX
           Time.parse(date)
         end
         
-        def build_balance
-          amount = html.search("ledgerbal > balamt").inner_text.to_f
+        def build_balance(account)
+          amount = account.search("ledgerbal > balamt").inner_text.to_f
           
           OFX::Balance.new({
             :amount => amount,
             :amount_in_pennies => (amount * 100).to_i,
-            :posted_at => build_date(html.search("ledgerbal > dtasof").inner_text)
+            :posted_at => build_date(account.search("ledgerbal > dtasof").inner_text)
           })
         end
     end
